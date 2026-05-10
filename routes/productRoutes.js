48d2e731474b6a6ada85f8378d183ac7cecef3dc
marc <marc_lawrence_delapena@dlsl.edu.ph>
2026-05-10 18:18:22 +0800

    third commit

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const productController = require('../controllers/productController');
-const authController = require('../controllers/authController');
 
 const router = express.Router();
 
@@ -22,6 +21,6 @@ router
   .route('/:id')
   .get(productController.getProduct)
   .patch(productController.updateProduct)
-  .delete(authController.protect, authController.restrictTo('admin'), productController.deleteProduct);
+  .delete(productController.deleteProduct);
 
 module.exports = router;
